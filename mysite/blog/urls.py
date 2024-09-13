6fc6c6955da5d30086f3d03cdc96aa59466e17fe
RafinadR <roma.veles@gmail.com>
2024-09-13 16:16:48 +0300

    Added views to create/edit blog post

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
